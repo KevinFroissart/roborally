e2712f5579b0c29f9073395f1939640d7a0b3349
Froissart Kevin <kevin.froissart69@gmail.com>
2021-02-28 20:00:36 +0100

    idée de sommet

---
+++ Src/headers/graphe.hpp
@@ -0,0 +1,14 @@
+#ifndef GRAPHE_HPP_
+#define GRAPHE_HPP_
+#include <map>
+#include <iostream>
+#include "sommet.hpp"
+
+class graphe
+{
+public:
+private:
+    std::map<std::string, sommet> map;
+};
+
+#endif
