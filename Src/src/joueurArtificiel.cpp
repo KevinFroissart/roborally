0f997b33f6f27753bba5cc19830c4208fd5eb186
Froissart Kevin <kevin.froissart69@gmail.com>
2021-04-01 17:35:17 +0200

    refactor draw method

--- Src/src/joueurArtificiel.cpp
+++ Src/src/joueurArtificiel.cpp
@@ -7,32 +7,7 @@ std::vector<RR::Robot::Move> JoueurArtificiel::tirage(int nbCarte)
     std::vector<RR::Robot::Move> tirage;
     srand(time(NULL));
     for (int i = 0; i < nbCarte; i++)
-    {
-        switch (rand() % 7 + 1)
-        {
-        case 1:
-            tirage.push_back(RR::Robot::Move::BACKWARD_1);
-            break;
-        case 2:
-            tirage.push_back(RR::Robot::Move::FORWARD_1);
-            break;
-        case 3:
-            tirage.push_back(RR::Robot::Move::FORWARD_2);
-            break;
-        case 4:
-            tirage.push_back(RR::Robot::Move::FORWARD_3);
-            break;
-        case 5:
-            tirage.push_back(RR::Robot::Move::TURN_LEFT);
-            break;
-        case 6:
-            tirage.push_back(RR::Robot::Move::TURN_RIGHT);
-            break;
-        case 7:
-            tirage.push_back(RR::Robot::Move::U_TURN);
-            break;
-        }
-    }
+        tirage.push_back((RR::Robot::Move)(rand() % 7));
     return tirage;
 }
 
