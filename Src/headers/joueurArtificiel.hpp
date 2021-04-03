23af585d806b7b9b59bed2c5367f6f29b09afdd3
Arthur <arthur.pradier81@gmail.com>
2021-04-03 17:02:09 +0200

    pseudo-code, overall structure

--- Src/headers/joueurArtificiel.hpp
+++ Src/headers/joueurArtificiel.hpp
@@ -8,7 +8,13 @@
 class JoueurArtificiel
 {
 public:
+    RR::Robot position;
+    RR::Robot objectif;
+    const int MAX_CARTES_MAIN;
+
+    JoueurArtificiel(RR::Robot position);
     std::vector<RR::Robot::Move> tirage(int nbCarte);
+    std::vector<RR::Robot::Move> JouerTour(std::vector<RR::Robot::Move> tirage, RR::Board board);
 
 private:
 };
--- Src/src/joueurArtificiel.cpp
+++ Src/src/joueurArtificiel.cpp
@@ -2,6 +2,10 @@
 #include <time.h>
 #include <stdlib.h>
 
+JoueurArtificiel::JoueurArtificiel(RR::Robot position) {
+    this->position = position;
+}
+
 std::vector<RR::Robot::Move> JoueurArtificiel::tirage(int nbCarte)
 {
     std::vector<RR::Robot::Move> tirage;
@@ -35,4 +39,31 @@ std::vector<RR::Robot::Move> JoueurArtificiel::tirage(int nbCarte)
         }
     }
     return tirage;
+}
+
+std::vector<RR::Robot::Move> JoueurArtificiel::JouerTour(std::vector<RR::Robot::Move> tirage, RR::Board board) {
+    RR::Robot posDepart = this->position;
+    std::vector<RR::Robot::Move> meilleurChoix;
+
+    //avec notre tirage, on cree un arbre à 9 branches representant les coups possibles. 
+    for(RR::Robot::Move move : tirage) {
+        //L'arbre aura une profondeur de 5 (on joue 5 coups par tour)
+        for (int i = 0; i < MAX_CARTES_MAIN; i++) {
+            //boucle sur toutes les cartes restantes que l'on peut jouer
+            //chaque branche aura un enfant de moins que la precedente (carte deja jouee)
+            board.play(posDepart, move);
+            //On va parcourir l'arbre afin de trouver quelle branche nous permet d'arriver soit a la case 
+            //objectif, soit a la case la plus proche de l'objectif (que l'on determine via un appel du plus court chemin)
+            if (posDepart.status == RR::Robot::Status::DEAD) 
+                break;
+            if (posDepart == objectif) {
+                //retourner la serie de coup qui a permis d'arriver a lo'bjectif, plus n'importes quelles autres
+                // si il y a moins de 5 coups
+            }
+
+        }
+        
+    }
+
+    return meilleurChoix;
 }
